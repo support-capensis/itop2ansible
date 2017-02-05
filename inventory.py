2585d7690120f7fcf02ae190bda49653ecd7448f
gotoole <gotoole@capensis.fr>
2017-02-05 23:14:14 +0100

    modifié :         .idea/workspace.xml 	modifié :         config.ini 	modifié :         inventory.py 	.idea/dictionaries/

--- inventory.py
+++ inventory.py
@@ -7,13 +7,13 @@ import re
 import json
 import argparse
 
-class itopobje(object):
+class ItopInventory(object):
     def __init__(self, configfile):
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         self.config = config
         try:
             with open(configfile) as f:
-                config.readfp(f)
+                config.read_file(f)
         except IOError:
             print("No config.ini file found")
         self.pattern = re.compile("^[a-zA-Z]+::[0-9]+$")
@@ -43,25 +43,29 @@ class itopobje(object):
         except requests.exceptions.ConnectionError:
             print("HTTP error unable to connect to Itop API")
 
-    def ifstrindata(str, data):
+    def get_itop_classes(self):
+        itopclass = self.config.sections()
+        return itopclass
+
+    def ifstrindata(self,str, data):
         if str in data:
             return data[str]
 
-    def find_str_dict(fstr, data):
+    def find_str_dict(self, fstr, data):
         findlist = []
         for key in data:
             if isinstance(data[key], dict):
-                findlist.append(ifstrindata(fstr, data[key]))
+                findlist.append(self.ifstrindata(fstr, data[key]))
                 devicedict = data[key]
-                find_str_dict(fstr, devicedict)
+                self.find_str_dict(fstr, devicedict)
             elif isinstance(data[key], list):
                 for i in data[key]:
-                    findlist.append(ifstrindata(fstr, i))
+                    findlist.append(self.ifstrindata(fstr, i))
                     if isinstance(i, dict):
-                        find_str_dict(fstr, i)
+                        self.find_str_dict(fstr, i)
         return findlist
 
-    def searchitopelem(httpreq):
+    def searchitopelem(self, httpreq):
         elem = []
         for i in httpreq:
             if i == "objects":
@@ -69,17 +73,20 @@ class itopobje(object):
                     elem.append(httpreq[i][a]['fields'])
         return elem
 
-objt = itopobje("config.ini")
-retreq = itopobje.send_request()
-result = searchitopelem(retreq)
-# find_str_dict("organization_name", retreq)
-for i in result:
-    print(i.get("name"))
-    print(i.get("organization_name"))
-    if "applicationsolution_id_friendlyname" not in i:
-        print(find_str_dict('applicationsolution_name', i))
-        # NONE NONE PROBLEM
-        # CHECK RETURN FOR DEEP SEARCH IN DICT
-        # find_str_dict("organization_name", retreq)
-        # print(json.dumps(retreq, indent=4))
-        # find_str_dict('applicationsolution_id_friendlyname', retreq)
+
+    def run(self):
+        pattern = re.compile("^class::[a-zA-Z]{1,}$")
+        for v in self.get_itop_classes():
+            if pattern.match(v):
+                configclass = v.split('::')
+                httpret = self.send_request(v, configclass)
+                dataelem = self.searchitopelem(httpret)
+                for srv in dataelem:
+                    print(srv)
+                    print(srv.get("name"))
+                    print(srv.get("organization_name"))
+                    if self.config.get(v, "test") not in srv:
+                        print(self.find_str_dict(self.config.get(v, "test"), srv))
+
+if __name__ == '__main__':
+    ItopInventory("config.ini").run()
