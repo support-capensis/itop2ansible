31314ead01ad9b03ff3d360e41a1db1ec092e08f
gotoole <gotoole@capensis.fr>
2017-02-05 21:03:19 +0100

    modifié :         .idea/workspace.xml 	modifié :         inventory.py 	config.ini

--- inventory.py
+++ inventory.py
@@ -7,19 +7,21 @@ import re
 import json
 import argparse
 
-class ItopToInventory(object):
-    def __init__(self, cfile):
-        config = configparser.ConfigParser()
+class itopobje(object):
+    def __init__(self, configfile):
+        config = ConfigParser.ConfigParser()
         self.config = config
-        with open(cfile) as f:
-            config.read_file(f)
+        try:
+            with open(configfile) as f:
+                config.readfp(f)
+        except IOError:
+            print("No config.ini file found")
         self.pattern = re.compile("^[a-zA-Z]+::[0-9]+$")
         self.url = self.config.get("itop", "url")
         self.user = self.config.get("itop", "user")
         self.passwd = self.config.get("itop", "passwd")
 
     def send_request(self, reqclass, defclass, sslverify=False):
-
         self.filter = self.config.get(reqclass, "filter")
         self.group_filter = self.config.get(reqclass, "group_filter").split(",")
         self.req = "{\"operation\":\"core/get\",\"" + defclass[0] + "\":\"" + defclass[1] + "\",\"key\":\"SELECT " + \
@@ -67,17 +69,17 @@ class ItopToInventory(object):
                     elem.append(httpreq[i][a]['fields'])
         return elem
 
-    otipinv = ItopToInventory("config.ini")
-    retreq = otipinv.send_request()
-    result = searchitopelem(retreq)
-    # find_str_dict("organization_name", retreq)
-    for i in result:
-        print(i.get("name"))
-        print(i.get("organization_name"))
-        if "applicationsolution_id_friendlyname" not in i:
-            print(find_str_dict('applicationsolution_name', i))
-            # NONE NONE PROBLEM
-            # CHECK RETURN FOR DEEP SEARCH IN DICT
-            # find_str_dict("organization_name", retreq)
-            # print(json.dumps(retreq, indent=4))
-            # find_str_dict('applicationsolution_id_friendlyname', retreq)
+objt = itopobje("config.ini")
+retreq = itopobje.send_request()
+result = searchitopelem(retreq)
+# find_str_dict("organization_name", retreq)
+for i in result:
+    print(i.get("name"))
+    print(i.get("organization_name"))
+    if "applicationsolution_id_friendlyname" not in i:
+        print(find_str_dict('applicationsolution_name', i))
+        # NONE NONE PROBLEM
+        # CHECK RETURN FOR DEEP SEARCH IN DICT
+        # find_str_dict("organization_name", retreq)
+        # print(json.dumps(retreq, indent=4))
+        # find_str_dict('applicationsolution_id_friendlyname', retreq)
