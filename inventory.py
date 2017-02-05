ec973e4e8f7208c289f7c870fb4e88f350bc1f71
gotoole <gotoole@capensis.fr>
2017-02-05 20:50:52 +0100

    nouveau fichier : .idea/itop2inventory.iml 	nouveau fichier : .idea/misc.xml 	nouveau fichier : .idea/modules.xml 	nouveau fichier : .idea/workspace.xml 	nouveau fichier : inventory.py

---
+++ inventory.py
@@ -0,0 +1,83 @@
+#/usr/bin/python3.4
+# coding: utf8
+import requests
+import json
+import configparser
+import re
+import json
+import argparse
+
+class ItopToInventory(object):
+    def __init__(self, cfile):
+        config = configparser.ConfigParser()
+        self.config = config
+        with open(cfile) as f:
+            config.read_file(f)
+        self.pattern = re.compile("^[a-zA-Z]+::[0-9]+$")
+        self.url = self.config.get("itop", "url")
+        self.user = self.config.get("itop", "user")
+        self.passwd = self.config.get("itop", "passwd")
+
+    def send_request(self, reqclass, defclass, sslverify=False):
+
+        self.filter = self.config.get(reqclass, "filter")
+        self.group_filter = self.config.get(reqclass, "group_filter").split(",")
+        self.req = "{\"operation\":\"core/get\",\"" + defclass[0] + "\":\"" + defclass[1] + "\",\"key\":\"SELECT " + \
+                   defclass[1] + "\",\"output_fields\":\"*\"}"
+        self.params = {"version": self.config.get("itop", "version"), "json_data": self.req}
+
+
+        if self.filter:
+            self.params["json_data"] = (self.params["json_data"].replace("*", self.filter))
+
+        if not sslverify:
+            from requests.packages.urllib3.exceptions import InsecureRequestWarning
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+        try:
+            r = requests.post(self.url, auth=(self.user, self.passwd), params=self.params, verify=False)
+            response = r.json()
+            return response
+        except requests.exceptions.ConnectionError:
+            print("HTTP error unable to connect to Itop API")
+
+    def ifstrindata(str, data):
+        if str in data:
+            return data[str]
+
+    def find_str_dict(fstr, data):
+        findlist = []
+        for key in data:
+            if isinstance(data[key], dict):
+                findlist.append(ifstrindata(fstr, data[key]))
+                devicedict = data[key]
+                find_str_dict(fstr, devicedict)
+            elif isinstance(data[key], list):
+                for i in data[key]:
+                    findlist.append(ifstrindata(fstr, i))
+                    if isinstance(i, dict):
+                        find_str_dict(fstr, i)
+        return findlist
+
+    def searchitopelem(httpreq):
+        elem = []
+        for i in httpreq:
+            if i == "objects":
+                for a in httpreq[i]:
+                    elem.append(httpreq[i][a]['fields'])
+        return elem
+
+    otipinv = ItopToInventory("config.ini")
+    retreq = otipinv.send_request()
+    result = searchitopelem(retreq)
+    # find_str_dict("organization_name", retreq)
+    for i in result:
+        print(i.get("name"))
+        print(i.get("organization_name"))
+        if "applicationsolution_id_friendlyname" not in i:
+            print(find_str_dict('applicationsolution_name', i))
+            # NONE NONE PROBLEM
+            # CHECK RETURN FOR DEEP SEARCH IN DICT
+            # find_str_dict("organization_name", retreq)
+            # print(json.dumps(retreq, indent=4))
+            # find_str_dict('applicationsolution_id_friendlyname', retreq)
